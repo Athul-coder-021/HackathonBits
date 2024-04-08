0557243052e3e1cd8596b6632ab1393be8aa5d53
Athul Krishna <ak021coder@gmail.com>
2024-04-08 12:49:10 +0530

    footer link updated

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -14,17 +14,17 @@ export default function Footer() {
           </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
             <li>
-              <Link to="http://localhost:5173/about" className="hover:text-yellow-300 me-4">About</Link>
+              <Link to="/about" className="hover:text-yellow-300 me-4">About</Link>
             </li>
             
             
             <li>
-              <Link to="/" className="hover:text-yellow-300">Contact</Link>
+              <Link to="/" className="hover:text-yellow-300">Home</Link>
             </li>
           </ul>
         </div>
         <hr className="my-6 border-gray-700" />
-        <span className="text-sm text-gray-500 sm:text-center">© 2024 </span><a href="http://localhost:5173/" className="hover:text-yellow-300">Nearby Now™</a>. <span>All Rights Reserved.</span>
+        <span className="text-sm text-gray-500 sm:text-center">© 2024 </span><a href="/" className="hover:text-yellow-300">Nearby Now™</a>. <span>All Rights Reserved.</span>
       </div>
     </footer>
   );
